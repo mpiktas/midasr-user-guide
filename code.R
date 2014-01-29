e799248323ff92981a9220c1dc06f1049afc9df6
Vaidotas Zemlys <zemlys@gmail.com>
2014-01-29 16:49:15 +0200

    Remove unnecessary code, add more info to README

--- code.R
+++ code.R
@@ -1,20 +1,3 @@
-graph <- function(x,pv0hac=NULL,titlestr=TRUE) {
-    cfur <- coef(x$unrestricted)
-    cfur <- cfur[grep("mls",names(cfur))]
-    cfre <- weight_coef(x)
-    k <- length(cfur)
-    sdval <- sqrt(diag(sandwich(x$unrestricted)))
-    sdval <- sdval[grep("mls",names(sdval))]
-    if(is.null(pv0hac))pv0hac <- hAhr.test(x)$p.value
-    plot(c(0:(k - 1)), c(cfur), col = "black", ylab = "Beta coefficients", xlab = "h")
-
-    if(titlestr) title(main = sprintf("p-val.(hAh_HAC) < %.2f", max(pv0hac, 0.01)), cex.main = 1, font.main = 4, col.main = "black")
-    
-    points(c(0:(k - 1)), cfre[1:k], type = "l", col = "blue")
-    points(c(0:(k - 1)), cfur[1:k] + 2 * sdval[1:k], type = "l", col = "red", lty = 2)
-    points(c(0:(k - 1)), cfur[1:k] - 2 * sdval[1:k], type = "l", col = "red", lty = 2)
-}
-
 harstep <- 
 function(p,d,m) {
     if(d!=20) stop("HAR(3)-RV process requires 20 lags")
