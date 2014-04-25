900716ae5517650fdf7378332a3ea6a990e89d7d
Vaidotas Zemlys <zemlys@gmail.com>
2014-04-25 16:43:28 +0300

    Make R output to adher to JSS style

--- code.R
+++
@@ -1,9 +0,0 @@
-harstep <- 
-function(p,d,m) {
-    if(d!=20) stop("HAR(3)-RV process requires 20 lags")
-    out <- rep(0,20)
-    out[1] <- p[1]+p[2]/5+p[3]/20
-    out[2:5] <- p[2]/5+p[3]/20
-    out[6:20] <- p[3]/20
-    out
-}
